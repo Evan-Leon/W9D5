786aff7d451ae2be0af9b879049c24f15d1c3fec
Joseph <Josephsipiorski@gmail.com>
2021-04-23 14:20:26 -0500

    adding in clock.js with phase 1

--- pocket_projects/src/index.js
+++ pocket_projects/src/index.js
@@ -1,6 +1,13 @@
 // import htmlGenerator from "./warmup"; //handles only one export
 import {htmlGenerator} from "./warmup"; // lines 2 and 3 handle many exports
 // import * as warmUp from "./warmup";
+import {Clock} from "./clock";
 
-const partyHeader = document.getElementById('party');
-htmlGenerator('Party Time.', partyHeader);
+// const partyHeader = document.getElementById('party');
+// htmlGenerator('Party Time.', partyHeader);
+
+// const clock = new Clock();
+// const clockElement = document.getElementById('clock');
+// htmlGenerator(clock, clockElement);
+
+new Clock();
